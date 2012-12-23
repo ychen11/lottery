96e59197b264cdec5f8fdc4092cb50bd73c1a600
Chen Yiwei <yiwei@peek.ly>
2012-12-23 00:08:11 -0500

    add NumbersGenerator.java

--- FirstBook/src/firstbook/QueryServlet.java
+++ FirstBook/src/firstbook/QueryServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 
+import util.NumbersGenerator;
 import util.Quick5Generator;
 import util.ResultGenerator;
 
@@ -18,14 +19,34 @@ public class QueryServlet extends HttpServlet {
 		resp.setContentType("text/plain");
 	//	resp.getWriter().println("Hello, this is QueryPage");
 		String type = req.getParameter("category");
+		if (type == null){
+			resp.getWriter().println("error: please set 'category' and 'param1'");
+			return;
+		}
 		if (type.equals("take5")){
 			resp.getWriter().println(QueryTake5(req.getParameter("param1")));
+		}
+		if (type.equals("numbers")){
+			resp.getWriter().println(QueryNumbers(req.getParameter("param1")));
 		}
 	}
 	
 	private String QueryTake5(String param){
 		if (!param.isEmpty()){
 			this._generator = new Quick5Generator(Integer.parseInt(param));
+			try {
+				return this._generator.getNumbers().toString();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return "Need round number";
+	}
+	
+	private String QueryNumbers(String param){
+		if (!param.isEmpty()){
+			this._generator = new NumbersGenerator(Integer.parseInt(param));
 			try {
 				return this._generator.getNumbers().toString();
 			} catch (JSONException e) {
---
+++ FirstBook/src/util/NumbersGenerator.java
@@ -0,0 +1,68 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
+public class NumbersGenerator extends ResultGenerator{
+
+	private int _roundNumber = 0;
+	private final int _rangeFrom = 0;
+	private final int _rangeTo = 9;
+	private ArrayList<ArrayList<Integer>> _numberList = null; 
+	private JSONObject _response = null;
+	
+	public NumbersGenerator(int roundnumber){
+		_roundNumber = roundnumber;
+		_numberList = new ArrayList<ArrayList<Integer>>();
+		_response = new JSONObject();
+	}
+
+	@Override
+	void GenNumberSequence() {
+		Hashtable<Integer, Integer> map= new Hashtable<Integer, Integer>();
+		map.clear();
+		while(_roundNumber > 0){
+			ArrayList<Integer> numbers = new ArrayList<Integer>();
+			for(int i = 0; i < 3; i++){
+				int value = this.GenerateNumber(_rangeFrom, _rangeTo);
+				if (map.containsValue(value) == false){
+					map.put(i, value);
+					numbers.add(value);
+				}else{
+					int newValue = this.GenerateNumber(_rangeFrom, _rangeTo);
+					while (map.containsValue(newValue) == true){
+						newValue = this.GenerateNumber(_rangeFrom, _rangeTo);
+					}
+					map.put(i, newValue);
+					numbers.add(newValue);
+				}
+			}
+			this._numberList.add(numbers);
+			_roundNumber--;
+		}
+		
+	}
+
+	@Override
+	public JSONObject getNumbers() throws JSONException {
+		// TODO Auto-generated method stub
+		GenNumberSequence();
+		if (_numberList.isEmpty() == true){
+			this._response.put("status", "empty");
+			return _response;
+		}
+		this._response.put("status", "full");
+		JSONArray numberArray = new JSONArray();
+		for (int i = 0; i < _numberList.size(); i++){
+			numberArray.put(_numberList.get(i));
+		}
+		_response.put("result", numberArray);
+		_response.put("category", "numbers");
+		return _response;
+	}
+	
+}
